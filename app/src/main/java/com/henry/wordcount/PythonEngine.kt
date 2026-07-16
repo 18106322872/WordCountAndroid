261a8d6d8a0782c72d0b45fb0e885f9537f36f19
18106322872 <1442828588@qq.com>
2026-07-16 19:04:39 +0800

    refactor: drop Py/PythonThread deps, use toJava + toString(None) check

--- app/src/main/java/com/henry/wordcount/PythonEngine.kt
+++ app/src/main/java/com/henry/wordcount/PythonEngine.kt
@@ -2,7 +2,6 @@ package com.henry.wordcount
 
 import android.content.Context
 import com.chaquo.python.Python
-import com.chaquo.python.Py
 import com.chaquo.python.android.AndroidPlatform
 
 /**
@@ -42,6 +41,7 @@ object PythonEngine {
         val py = Python.getInstance()
         val mod = py.getModule("wordcount")
         val result = mod.callAttr("build_export_pdf", filesInfo, outPath)
-        return if (result == Py.None) null else result.toString()
+        val s = result.toString()
+        return if (s == "None") null else s
     }
 }
